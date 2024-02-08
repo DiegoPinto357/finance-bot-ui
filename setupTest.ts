ed08163a168d59200071b575206772efe582874b
DiegoPintoTW <diego.dossantos@thoughtworks.com>
2024-02-08 11:41:32 -0300

    Setup vistest

---
+++ setupTest.ts
@@ -0,0 +1,10 @@
+import { expect, afterEach } from 'vitest';
+import 'vitest-dom/extend-expect';
+import { cleanup } from '@testing-library/react';
+import * as matchers from 'vitest-dom/matchers';
+
+expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
@@ -5,6 +8,14 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setupTest.ts',
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
