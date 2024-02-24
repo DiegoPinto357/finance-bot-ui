4f07795d7986442c0019a2f0f06d2f66c84fc22e
DiegoPintoTW <diego.dossantos@thoughtworks.com>
2024-02-24 16:31:49 -0300

    Refactor react dnd mock

--- __mocks__/react-dnd.ts
+++ __mocks__/react-dnd.ts
@@ -1,21 +1,9 @@
 import { vi } from 'vitest';
 
-type DragFunc = () => void;
-
-let dragFunc: () => void;
-
-export const useDrag = vi.fn().mockImplementation((func: DragFunc) => {
-  dragFunc = func;
-  return [vi.fn(), vi.fn(), vi.fn()];
-});
+export const useDrag = vi
+  .fn()
+  .mockImplementation(() => [vi.fn(), vi.fn(), vi.fn()]);
 
 export const useDrop = vi
   .fn()
   .mockImplementation(() => [vi.fn(), vi.fn(), vi.fn()]);
-
-export const triggerDrag = () => {
-  if (dragFunc) {
-    const dragData = dragFunc();
-    console.log({ dragData });
-  }
-};
