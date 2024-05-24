32768d8e6b6f06cf2fa9ccedf1fa5febd45d8544
DiegoPintoTW <diego.dossantos@thoughtworks.com>
2024-05-24 18:50:49 -0300

    Refactor typography

--- src/components/lib/Typography.tsx
+++ src/components/lib/Typography.tsx
@@ -3,12 +3,14 @@ import type { PropsWithChildren } from 'react';
 type Props = {
   variant: 'h1' | 'h2' | 'h3' | 'h4' | 'p';
   className?: string;
+  id?: string;
 };
 
 const Typography = ({
   variant,
   children,
   className,
+  ...rest
 }: PropsWithChildren<Props>) => {
   switch (variant) {
     case 'h1':
@@ -16,6 +18,7 @@ const Typography = ({
         <h1
           // TODO prevent "undefined" className (check other places)
           className={`scroll-m-16 font-semibold mb-4 text-4xl lg:text-5xl lg:mb-8 ${className}`}
+          {...rest}
         >
           {children}
         </h1>
@@ -25,6 +28,7 @@ const Typography = ({
       return (
         <h2
           className={`scroll-m-8 font-semibold mb-2 text-2xl lg:text-3xl lg:mb-4 ${className}`}
+          {...rest}
         >
           {children}
         </h2>
@@ -34,6 +38,7 @@ const Typography = ({
       return (
         <h3
           className={`scroll-m-4 font-semibold mb-1 text-lg lg:text-xl lg:mb-2 ${className}`}
+          {...rest}
         >
           {children}
         </h3>
@@ -43,13 +48,18 @@ const Typography = ({
       return (
         <h4
           className={`scroll-m-2 mb-0.5 text-md lg:text-lg lg:mb-1 ${className}`}
+          {...rest}
         >
           {children}
         </h4>
       );
 
     case 'p':
-      return <p className={`leading-6  ${className}`}>{children}</p>;
+      return (
+        <p className={`leading-6  ${className}`} {...rest}>
+          {children}
+        </p>
+      );
   }
 };
 
