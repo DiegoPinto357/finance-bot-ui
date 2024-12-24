a624bcdfae521fcb2ea107a7c432f96018ae7a9d
DiegoPintoTW <diego.dossantos@thoughtworks.com>
2024-12-23 23:51:12 -0300

    Remove unused settings placeholders

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -10,7 +10,7 @@ const Settings = () => {
       <Status className="my-8" />
       <Configs className="my-8" />
 
-      <div className="my-8">
+      {/* <div className="my-8">
         <Typography variant="h2">Actions</Typography>
         Update tables Clear cache
       </div>
@@ -18,7 +18,7 @@ const Settings = () => {
       <div className="my-8">
         <Typography variant="h2">Debug</Typography>
         Dryrun mode Data cache config
-      </div>
+      </div> */}
     </>
   );
 };
